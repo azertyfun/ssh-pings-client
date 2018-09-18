f87c7ecc4b982c3a3a99cba2ac73c724aafc0827
azertyfun <azertyfun_poubelle@hotmail.fr>
2018-09-18 20:34:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the client for ssh-pings
---
+++ index.js
@@ -0,0 +1,94 @@
+// country.io/iso3.json
+var iso3166_alpha2_to_alpha3 = {"BD": "BGD", "BE": "BEL", "BF": "BFA", "BG": "BGR", "BA": "BIH", "BB": "BRB", "WF": "WLF", "BL": "BLM", "BM": "BMU", "BN": "BRN", "BO": "BOL", "BH": "BHR", "BI": "BDI", "BJ": "BEN", "BT": "BTN", "JM": "JAM", "BV": "BVT", "BW": "BWA", "WS": "WSM", "BQ": "BES", "BR": "BRA", "BS": "BHS", "JE": "JEY", "BY": "BLR", "BZ": "BLZ", "RU": "RUS", "RW": "RWA", "RS": "SRB", "TL": "TLS", "RE": "REU", "TM": "TKM", "TJ": "TJK", "RO": "ROU", "TK": "TKL", "GW": "GNB", "GU": "GUM", "GT": "GTM", "GS": "SGS", "GR": "GRC", "GQ": "GNQ", "GP": "GLP", "JP": "JPN", "GY": "GUY", "GG": "GGY", "GF": "GUF", "GE": "GEO", "GD": "GRD", "GB": "GBR", "GA": "GAB", "SV": "SLV", "GN": "GIN", "GM": "GMB", "GL": "GRL", "GI": "GIB", "GH": "GHA", "OM": "OMN", "TN": "TUN", "JO": "JOR", "HR": "HRV", "HT": "HTI", "HU": "HUN", "HK": "HKG", "HN": "HND", "HM": "HMD", "VE": "VEN", "PR": "PRI", "PS": "PSE", "PW": "PLW", "PT": "PRT", "SJ": "SJM", "PY": "PRY", "IQ": "IRQ", "PA": "PAN", "PF": "PYF", "PG": "PNG", "PE": "PER", "PK": "PAK", "PH": "PHL", "PN": "PCN", "PL": "POL", "PM": "SPM", "ZM": "ZMB", "EH": "ESH", "EE": "EST", "EG": "EGY", "ZA": "ZAF", "EC": "ECU", "IT": "ITA", "VN": "VNM", "SB": "SLB", "ET": "ETH", "SO": "SOM", "ZW": "ZWE", "SA": "SAU", "ES": "ESP", "ER": "ERI", "ME": "MNE", "MD": "MDA", "MG": "MDG", "MF": "MAF", "MA": "MAR", "MC": "MCO", "UZ": "UZB", "MM": "MMR", "ML": "MLI", "MO": "MAC", "MN": "MNG", "MH": "MHL", "MK": "MKD", "MU": "MUS", "MT": "MLT", "MW": "MWI", "MV": "MDV", "MQ": "MTQ", "MP": "MNP", "MS": "MSR", "MR": "MRT", "IM": "IMN", "UG": "UGA", "TZ": "TZA", "MY": "MYS", "MX": "MEX", "IL": "ISR", "FR": "FRA", "IO": "IOT", "SH": "SHN", "FI": "FIN", "FJ": "FJI", "FK": "FLK", "FM": "FSM", "FO": "FRO", "NI": "NIC", "NL": "NLD", "NO": "NOR", "NA": "NAM", "VU": "VUT", "NC": "NCL", "NE": "NER", "NF": "NFK", "NG": "NGA", "NZ": "NZL", "NP": "NPL", "NR": "NRU", "NU": "NIU", "CK": "COK", "XK": "XKX", "CI": "CIV", "CH": "CHE", "CO": "COL", "CN": "CHN", "CM": "CMR", "CL": "CHL", "CC": "CCK", "CA": "CAN", "CG": "COG", "CF": "CAF", "CD": "COD", "CZ": "CZE", "CY": "CYP", "CX": "CXR", "CR": "CRI", "CW": "CUW", "CV": "CPV", "CU": "CUB", "SZ": "SWZ", "SY": "SYR", "SX": "SXM", "KG": "KGZ", "KE": "KEN", "SS": "SSD", "SR": "SUR", "KI": "KIR", "KH": "KHM", "KN": "KNA", "KM": "COM", "ST": "STP", "SK": "SVK", "KR": "KOR", "SI": "SVN", "KP": "PRK", "KW": "KWT", "SN": "SEN", "SM": "SMR", "SL": "SLE", "SC": "SYC", "KZ": "KAZ", "KY": "CYM", "SG": "SGP", "SE": "SWE", "SD": "SDN", "DO": "DOM", "DM": "DMA", "DJ": "DJI", "DK": "DNK", "VG": "VGB", "DE": "DEU", "YE": "YEM", "DZ": "DZA", "US": "USA", "UY": "URY", "YT": "MYT", "UM": "UMI", "LB": "LBN", "LC": "LCA", "LA": "LAO", "TV": "TUV", "TW": "TWN", "TT": "TTO", "TR": "TUR", "LK": "LKA", "LI": "LIE", "LV": "LVA", "TO": "TON", "LT": "LTU", "LU": "LUX", "LR": "LBR", "LS": "LSO", "TH": "THA", "TF": "ATF", "TG": "TGO", "TD": "TCD", "TC": "TCA", "LY": "LBY", "VA": "VAT", "VC": "VCT", "AE": "ARE", "AD": "AND", "AG": "ATG", "AF": "AFG", "AI": "AIA", "VI": "VIR", "IS": "ISL", "IR": "IRN", "AM": "ARM", "AL": "ALB", "AO": "AGO", "AQ": "ATA", "AS": "ASM", "AR": "ARG", "AU": "AUS", "AT": "AUT", "AW": "ABW", "IN": "IND", "AX": "ALA", "AZ": "AZE", "IE": "IRL", "ID": "IDN", "UA": "UKR", "QA": "QAT", "MZ": "MOZ"}
+
+function update(data) {
+    $.each(data.pings, function(i, ping) {
+        if (ping.timestamp != last_ping.timestamp && ping.origin != "unknown") {
+            console.log('Ping from ' + ping.origin.country);
+
+            pings.push(ping);
+
+            let alpha3 = iso3166_alpha2_to_alpha3[ping.origin.country]; // GeoJSON uses ISO3166-Alpha3 codes while everybody else uses ISO3166-Alpha2 like reasonable people
+            let n = 0;
+            if (!(alpha3 in countries)) {
+                n = 1;
+                countries[alpha3] = 1;
+            } else {
+                n = countries[alpha3] + 1;
+                countries[alpha3]++;
+            }
+
+            // Update Choropleth
+
+            newChoropleth = {};
+            newChoropleth[alpha3] = n;
+            map.updateChoropleth(newChoropleth);
+
+            // Create/Update arc
+
+            let strokeWidth = 1.0;
+            $.each(pings, function(i, p) {
+                if (p.origin.lat === ping.origin.lat && p.origin.lon === ping.origin.lon) {
+                    strokeWidth += 1.5;
+                }
+            });
+
+            arcs.push({
+                origin: {
+                    latitude: ping.origin.lat,
+                    longitude: ping.origin.lon
+                },
+                destination: {
+                    latitude: 50.85,
+                    longitude: 4.35
+                },
+                strokeWidth: strokeWidth
+            });
+
+            map.arc(arcs, {
+                animationSpeed: 1200
+            })
+        }
+    });
+
+    last_ping = data.pings[data.pings.length - 1];
+
+    url = '/ssh-pings-backend/since/' + last_ping.timestamp;
+
+    window.setTimeout(function () {
+        $.ajax({
+            dataType: 'json',
+            url: url,
+            success: update
+        })
+        // $.getJSON(url, update);
+    }, 1000);
+}
+
+var map = new Datamap({
+    element: document.getElementById('container'),
+    responsive: true,
+    fills: {
+        HIGH: 'rgb(255, 69, 23)',
+        MEDIUM: 'rgb(255, 69, 23)',
+        LOW: 'rgb(230, 230, 230)',
+        UNKNOWN: 'rgb(128, 128, 128)',
+        defaultFill: 'rgb(230, 230, 230)'
+    }
+});
+
+$(window).on('resize', function() {
+    map.resize();
+});
+
+var pings = [];
+var last_ping = {};
+var countries = {};
+var arcs = [];
+$.getJSON("/ssh-pings-backend/last", function(data) {
+    last_ping = data;
+
+    window.setTimeout(function () {
+        $.getJSON('/ssh-pings-backend/since/' + last_ping.timestamp, update);
+    }, 1000);
+})
